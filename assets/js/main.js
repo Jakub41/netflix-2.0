4c25e1fe1fd0cbbaa68eb300d51f2815a2b9c649
jakub41 <lemiszewski@gmx.com>
2019-10-27 16:37:55 +0100

    Revert of the modular attempt via Jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,27 +3,29 @@
  * Animated Enter Message Container
  *
  */
-// We set a timeout to show the div
-console.log("Am I running??");
-setTimeout(function() {
-    $("#n-animation").show();
-    centerDiv();
-}, 3000);
+$(document).ready(function() {
+    // We set a timeout to show the div
+    console.log("Am I running??");
+    setTimeout(function() {
+        $("#n-animation").show();
+        centerDiv();
+    }, 3000);
 
-// We center the div in the page
-function centerDiv() {
-    var windowWidth = $(window).width();
-    var windowHeight = $(window).height();
+    // We center the div in the page
+    function centerDiv() {
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
 
-    var divWidth = $("#n-animation").width();
-    var divHeight = $("#n-animation").height();
+        var divWidth = $("#n-animation").width();
+        var divHeight = $("#n-animation").height();
 
-    $("#n-animation")
-        .css("top", windowHeight / 2 - divHeight / 2)
-        .css("left", windowWidth / 2 - divWidth / 2);
-}
+        $("#n-animation")
+            .css("top", windowHeight / 2 - divHeight / 2)
+            .css("left", windowWidth / 2 - divWidth / 2);
+    }
 
-// Resize called every time we try to resize the window
-$(window).resize(function() {
-    centerDiv();
+    // Resize called every time we try to resize the window
+    $(window).resize(function() {
+        centerDiv();
+    });
 });
